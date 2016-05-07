ea813152dc63c050f64c102afc816f00b096b4b8
Agustin <amora@princeton.edu>
2016-05-07 15:47:16 -0400

    updates

--- server.py
+++ server.py
@@ -1,23 +1,61 @@
+from __future__ import print_function # In python 2.7
 import os
+import sys
+import utils
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World!'
+tag = None
+image_queue = None
+zero_pics = 0
+one_pics = 0
+two_pics = 0
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello_name(name=None, pic=None):
-   return render_template('hello.html', name=name, pic="https://scontent-ord1-1.cdninstagram.com/t51.2885-15/e35/13150963_603120863177545_6030527_n.jpg")
+@app.route('/')
+def start_page():
+   return render_template('start_page.html')
+
+@app.route('/insertTag', methods=['POST'])
+def insert():
+   if request.method == 'POST':
+      text = request.form['text']
+      if len(text) > 0:
+         global tag, zero_pics, one_pics, two_pics
+         tag = text
+         zero_pics = 0
+         one_pics = 0
+         two_pics = 0
+   return redirect("/pics")
+
+@app.route('/pics')
+def pics():
+   global image_queue
+   if image_queue == None:
+      image_queue = utils.getUrls(utils.getMedia(tag))
+   pic = image_queue.pop()
+   print(pic, file=sys.stderr)
+   return render_template('rate_pics.html', tag=tag, pic=pic)
 
 @app.route('/rate', methods=['GET', 'POST'])
-def login():
+def rate():
+   global zero_pics, one_pics, two_pics
    if request.method == 'POST':
-      do_the_login()
-   else:
-      show_the_login_form()
+      value = request.form["rate"]
+      print(value, file=sys.stderr)
+      if value == '0':
+         zero_pics += 1
+      elif value == '1':
+         one_pics += 1
+      else:
+         two_pics += 1
+   return redirect("/pics")
+
+@app.route('/results')
+def results():
+   return render_template('results.html', zero=zero_pics, one=one_pics, two=two_pics)
+
+
 
 if __name__ == '__main__':
    app.run()
--- utils.py
+++ utils.py
@@ -1,14 +1,66 @@
+from collections import Counter
+import json
+import urllib2
+import sys
 
+# Login Credentials
+access_token = "1014426274.1677ed0.a695406addfa4f2c8de879708912e5a0"
+client_id = "4f4b027e014540948cf8b6acbc88c2bb"
+client_secret = "13e9e272df824a7895db6b9747f2ef31"
+
+# Image class to store image urls and tags
+class Image:
+   def __init__(self, media):
+      self.url = media["link"]
+      self.tags = [i.encode('utf-8') for i in media["tags"]]
+
+   def toString(self):
+      print "Link: " + self.url
+      print "Tags:",
+      for t in self.tags:
+         print t,
+
+# Function to return images and there tags
+def getMedia(tag):
+   url = 'https://api.instagram.com/v1/tags/%s/media/recent?access_token=%s' % (tag, access_token)
+   media = json.load(urllib2.urlopen(url))["data"]
+   
+   images = dict()
+   for i in media:
+      if i["link"] not in images:
+         images[i["link"]] = Image(i)
+   return images
+
+def get_pic():
+   return "https://scontent-ord1-1.cdninstagram.com/t51.2885-15/e35/13150963_603120863177545_6030527_n.jpg"
 
 def getPhotoUrl(url):
-   import urllib2
    html = urllib2.urlopen(url).read()
    parts = html.partition("<meta property=\"og:image\" content=\"")[2].partition(".jpg")
    image_url = parts[0] + parts[1]
-   print image_url
+   return image_url
 
-def main():
-   pass
+def getUrls(images):
+   return [getPhotoUrl(images[i].url) for i in images]
+
+def printImages(images, count):
+   c = 0
+   for i in images:
+      if c == count:
+         break
+      print images[i].toString()
+      c += 1
+
+
+
+
+def main(args):
+   if len(args) < 1:
+      sys.exit()
+   if args[0] == "media":
+      printImages(getMedia(args[1]), 20)
+   if args[0] == "url":
+      print getUrls(getMedia(args[1]))
 
 if __name__ == '__main__':
-   main()
+   main(sys.argv[1:])
